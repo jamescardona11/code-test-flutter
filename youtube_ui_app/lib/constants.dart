d3e70b29595732506e8e1c4d11127aa0e1c16af1
jamescardona11 <jjcardonao@gmail.com>
2020-09-03 16:58:38 -0500

    item for videos

---
+++ youtube_ui_app/lib/constants.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+class GeneratorText {
+  static InfoVideo getRandom() => mapGenerator[Random().nextInt(mapGenerator.length)];
+
+  static final mapGenerator = [
+    InfoVideo('Luisito Comunica', 'Así es el castillo de dracula', '1.6M', '1 year ago', '12:05'),
+    InfoVideo('MovieClips', 'Man of steel', '7.2M', '6 months ago', '21:15'),
+    InfoVideo('Fireship', '7 Database Paradigms', '10K', '5 hours ago', '11:08'),
+    InfoVideo('SensaCine', 'Greenland Trailer', '25.1K', '1 week ago', '7:05'),
+    InfoVideo('TecnoRR', 'The Best headphone under 300', '250.1K', '3 week ago', '32:05'),
+  ];
+}
+
+class InfoVideo {
+  final String nameUser;
+  final String nameVideo;
+  final String views;
+  final String time;
+  final String duration;
+
+  InfoVideo(this.nameUser, this.nameVideo, this.views, this.time, this.duration);
+
+  String get concatValues => '$nameUser · $views · $time';
+}
---
+++ youtube_ui_app/lib/home_view.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_ui_app/constants.dart';
+
+const kLoadingImage = 'assets/circular_loading.gif';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _ItemList(),
+      ),
+    );
+  }
+}
+
+class _ItemList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final infoVideo = GeneratorText.getRandom();
+    return Container(
+      width: size.width,
+      height: size.height * 0.45,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Flexible(
+            child: Container(
+              color: Colors.green,
+              child: FadeInImage(
+                placeholder: AssetImage(kLoadingImage),
+                image: NetworkImage('https://picsum.photos/500/300'),
+              ),
+              width: size.width,
+            ),
+            flex: 3,
+          ),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  _ImageProfile(),
+                  SizedBox(width: 15),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        infoVideo.nameVideo,
+                        style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        infoVideo.concatValues,
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Expanded(child: SizedBox()),
+                  Icon(Icons.more_vert)
+                ],
+              ),
+            ),
+            flex: 1,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _ImageProfile extends StatelessWidget {
+  const _ImageProfile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(100),
+      child: FadeInImage(
+        placeholder: AssetImage(kLoadingImage),
+        image: NetworkImage('https://picsum.photos/200/300'),
+        width: 55,
+        height: 55,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
