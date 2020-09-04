26583dbf2fe57a24e58b61b70be1321a417e533e
jamescardona11 <jjcardonao@gmail.com>
2020-09-03 19:57:37 -0500

    ActionBar

--- youtube_ui_app/lib/main.dart
+++ youtube_ui_app/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:youtube_ui_app/constants.dart';
 import 'package:youtube_ui_app/provider/navigation_provider.dart';
-import 'package:youtube_ui_app/views/details_video_view.dart';
 import 'package:youtube_ui_app/views/viewpage_parent_view.dart';
 
 void main() => runApp(MyApp());
@@ -16,6 +14,38 @@ class MyApp extends StatelessWidget {
       home: ChangeNotifierProvider(
         create: (_) => NavigationProvider(),
         child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            leading: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset('assets/youtube.png'),
+            ),
+            title: Text(
+              'Youtube',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Image.asset(
+                  'assets/imageprofile.jpg',
+                  width: 35,
+                  height: 35,
+                ),
+              ),
+            ],
+          ),
           body: ViewPageParentView(),
         ),
       ),
